df2e0a3b4504014d2a0fc8c284cd73d58a91ede3
daniel cheang <daniel@cpttm.org.mo>
2023-10-27 12:58:30 +0800

    scroll to el

--- src/components/HList.vue
+++ src/components/HList.vue
@@ -3,11 +3,21 @@ import HList from './HList.vue'
 defineProps({
   node: Object
 })
+
+function scrollTo(id) {
+  const el = document.querySelector('#' + id)
+  if (!el) return
+  const { top } = el.getBoundingClientRect()
+  window.scrollTo({
+    top: top - 80,
+    behavior: 'smooth'
+  })
+}
 </script>
 <template>
   <ul v-if="node">
     <li v-for="c in node.children" :key="c">
-      <a :href="`#${c.id}`">{{ c.text }}</a>
+      <a href="#" @click.prevent="scrollTo(c.id)">{{ c.text }}</a>
       <h-list :node="c"></h-list>
     </li>
   </ul>
